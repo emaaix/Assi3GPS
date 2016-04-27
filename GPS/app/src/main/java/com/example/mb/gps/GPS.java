a3cfbafeb4adff1b3ae285f65363821b98fa314c
Max Beauvais <Max Beauvais>
2016-04-27 14:36:00 +0200

    milestone 6

--- GPS/app/src/main/java/com/example/mb/gps/GPS.java
+++ GPS/app/src/main/java/com/example/mb/gps/GPS.java
@@ -27,12 +27,15 @@ public class GPS extends AppCompatActivity {
     Location location;
     public static Location[] localist = new Location[30];
     LocationListener localistener;
-    int ind;
+    public static int ind;
     LocationManager locationManager;
     TextView t1;
+    public static TextView t2;
+    TextView t3;
+    TextView t4;
     boolean act = false;
     Button start;
-
+    int clock = 0;
 
 
     @Override
@@ -44,57 +47,63 @@ public class GPS extends AppCompatActivity {
 
 
         start = (Button) findViewById(R.id.start_button);
-        t1= (TextView) findViewById(R.id.textView2);
+        t1= (TextView) findViewById(R.id.textView);
+        t2= (TextView) findViewById(R.id.textView2);
+        t3= (TextView) findViewById(R.id.textView3);
+        t4= (TextView) findViewById(R.id.textView4);
 
 
 
         start.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if (start.getText() == "Start Tracking") {
-                    start.setText("Stop Tracking");
-                    try {
-                        localistener = new LocationListener() {
+                    if (start.getText() == "Start Tracking") {
+                        start.setText("Stop Tracking");
+                        t4.setText("GPS Active");
+                        try {
+                            localistener = new LocationListener() {
 
-                            @Override
-                            public void onLocationChanged(Location location) {
-                                localist[ind] = location;
-                                t1.setText(localist[ind].toString());
-                                ind++;
-                                if (ind == 30) {
-                                    ind = 0;
+                                @Override
+                                public void onLocationChanged(Location location) {
+                                    clock++;
+                                    t1.setText("Overall Time : " + clock + "s");
+                                    t3.setText("Current Speed : " + location.getSpeed());
+                                    localist[ind] = location;
+                                    ind++;
+                                    if (ind == 30) {
+                                        ind = 0;
+                                    }
+
                                 }
 
-                            }
+                                @Override
+                                public void onStatusChanged(String provider, int status, Bundle extras) {
 
-                            @Override
-                            public void onStatusChanged(String provider, int status, Bundle extras) {
+                                }
 
-                            }
+                                @Override
+                                public void onProviderEnabled(String provider) {
 
-                            @Override
-                            public void onProviderEnabled(String provider) {
+                                }
 
-                            }
+                                @Override
+                                public void onProviderDisabled(String provider) {
 
-                            @Override
-                            public void onProviderDisabled(String provider) {
-
-                            }
-                        };
-                    }
-                    catch (Exception e)
-                    {
-                        Log.e("EFG","falbala");
-                    }
-                    try {
-                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, localistener);
-                    }
-                    catch (Exception e)
-                    {}
+                                }
+                            };
+                        } catch (Exception e) {
+                            Log.e("EFG", "falbala");
+                        }
+                        try {
+                            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, localistener);
+                        } catch (Exception e) {
+                        }
                 }
                 else{
                     start.setText("Start Tracking");
-                    act = false;
+                    t4.setText("GPS Inactive");
+                    t3.setText("Current Speed : N/A");
+                    t2.setText("Averrage Speed : N/A");
+                    t1.setText("Overall Time : 0s");
                     for(int i =0 ; i < 30; i++)
                     {
                         localist[i] = null;
--- GPS/app/src/main/java/com/example/mb/gps/Graph.java
+++ GPS/app/src/main/java/com/example/mb/gps/Graph.java
@@ -53,13 +53,28 @@ public class Graph extends View {
         can.drawLine(0, size - 1, size, size - 1, paint);
         Paint paint2 = new Paint();
         paint2.setColor(Color.GREEN);
+        float tata= (float) 3.6;
         for(int i= 0; i<29; i++)
         {
             if (GPS.localist[i] != null && GPS.localist[i+1]!=null)
             {
-                can.drawLine((size/30)*i,size - (GPS.localist[i].getSpeed()*size)/60,(size/30)*(i+1),size-(GPS.localist[i+1].getSpeed()*size)/60,paint2);
+                can.drawLine((size/30)*i, (float) ((size - (GPS.localist[i].getSpeed()*size)/60)),(size/30)*(i+1), (float) ((size-(GPS.localist[i+1].getSpeed()*size)/60)),paint2);
             }
         }
+        float speed = 0;
+
+        for(int j = 0; j<GPS.ind; j++)
+        {
+            if (GPS.localist[j] != null)
+            {
+                speed += GPS.localist[j].getSpeed()*3.6f;
+            }
+        }
+        speed = speed/GPS.ind;
+        Paint paint3 = new Paint();
+        paint3.setColor(Color.RED);
+        can.drawLine(0, (speed * size) / 90, size, (speed * size) / 90, paint3);
+        GPS.t2.setText("Averrage Speed : "+speed);
         invalidate();
     }
 }
